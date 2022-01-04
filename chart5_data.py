0efd1b394b5407157358a09b8b71c2b2f89808a1
eli.meyer <elimeyer6@gmail.com>
2022-01-04 13:30:29 +0000

    Uploading stocks data

---
+++ chart5_data.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+pd.set_option("display.max_rows", 1000, "display.max_columns", 1000)
+
+tickers = ["FB", "AAPL", "AMZN", "NFLX", "GOOGL"]
+start = "2017-01-04"
+end = "2022-01-04"
+
+df1 = yf.download(tickers, start, end)["Close"]
+df1.columns=["FB", "AAPL", "AMZN", "NFLX", "GOOGL"]
+
+df1.to_csv(r"C:\Users\meyer\github\EliezerMeyer.github.io\stocks.csv")
