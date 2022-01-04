9e6759d84a0793ddd998f0de3084294eb226dad1
eli.meyer <elimeyer6@gmail.com>
2022-01-04 14:27:33 +0000

    Adding the s&p500

--- chart5_data.py
+++ chart5_data.py
@@ -4,10 +4,11 @@ import numpy as np
 
 pd.set_option("display.max_rows", 1000, "display.max_columns", 1000)
 
-tickers = ["FB", "AAPL", "AMZN", "NFLX"]
+tickers = ["FB", "AAPL", "AMZN", "GOOGL", "NFLX", "^GSPC"]
 start = "2017-01-04"
 end = "2022-01-04"
 
+
 df1 = yf.download(tickers, start, end)["Close"]
 
-df1.to_csv(r"C:\Users\meyer\github\EliezerMeyer.github.io\stocks_corrected.csv")
+df1.to_csv(r"C:\Users\meyer\github\EliezerMeyer.github.io\stocks.csv")
