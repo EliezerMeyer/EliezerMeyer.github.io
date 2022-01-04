ddad21ea4a0c8d7e85a2903b6517ad5514c16048
eli.meyer <elimeyer6@gmail.com>
2022-01-04 13:37:22 +0000

    Checking

--- chart5_data.py
+++ chart5_data.py
@@ -9,6 +9,5 @@ start = "2017-01-04"
 end = "2022-01-04"
 
 df1 = yf.download(tickers, start, end)["Close"]
-print(df1)
 
-df1.to_csv(r"C:\Users\meyer\github\EliezerMeyer.github.io\stocks.csv")
+df1.to_csv(r"C:\Users\meyer\github\EliezerMeyer.github.io\stocks_corrected.csv")
