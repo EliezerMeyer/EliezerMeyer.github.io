4a2dcb7156aac10b8b14780646bea14800653aac
eli.meyer <elimeyer6@gmail.com>
2022-01-04 20:46:05 +0000

    Uploading Israel Innovation Data

---
+++ Israelinnovationdata.py
@@ -0,0 +1,18 @@
+# I will be using Israel as a case study of innovation, since it has an extremely high spend on R&D as a percentage of GDP
+# I will first create a bar chart showing this and that the figure has been increasing steadily
+
+# Importing necessary modules, downloading data directly from the World Bank
+import world_bank_data as wb
+import pandas as pd
+
+# Allowing myself to see the whole dataframe
+
+pd.set_option("display.max_rows", 1000, "display.max_columns", 1000)
+
+# Working on the dataframe
+
+df1 = pd.DataFrame(wb.get_series('GB.XPD.RSDV.GD.ZS', date='1996:2018', id_or_value='id', country="ISR", simplify_index=True))
+
+# Saving it to be charted
+
+df1.to_csv("Israeldata.csv")
